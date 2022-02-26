01704dc45fbcaaf1fe07e07c08ac91414f704795
jmajaca <jmajaca@croz.net>
2022-02-26 21:00:01 +0100

    [JAVA-BACKEND-31] Init image-api

---
+++ image-api/src/main/java/hr/show/ImageApiApplication.java
@@ -0,0 +1,13 @@
+package hr.show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageApiApplication.class, args);
+    }
+
+}
