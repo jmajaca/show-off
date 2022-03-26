ebe559f1e3a8e688fbdef6ca85564de9498511ba
jmajaca <josip.majaca@gmail.com>
2022-03-26 16:12:59 +0100

    [PYTHON-BACKEND-43] Detection API documentation

--- detection/python/src/api_schemas.py
+++ detection/python/src/api_schemas.py
@@ -3,20 +3,31 @@ from marshmallow import Schema, fields
 
 class PointSchema(Schema):
 
-    axis_x = fields.Int()
-    axis_y = fields.Int()
+    axis_x = fields.Int(metadata={'description': 'x-axis coordinate of point'})
+    axis_y = fields.Int(metadata={'description': 'y-axis coordinate of point'})
+
+
+class TextBoxRequestSchema(Schema):
+
+    image = fields.Raw(metadata={'description': 'Image in bytes'})
 
 
 class TextBoxSchema(Schema):
 
-    ord_num = fields.Int()
-    points = fields.List(fields.Nested(PointSchema))
-    probability = fields.Float()
+    ord_num = fields.Int(metadata={'description': 'Ordinal number of box'})
+    points = fields.List(fields.Nested(PointSchema), metadata={'description': 'Points that determine box'})
+    probability = fields.Float(metadata={'description': 'Probability of box containing text'})
 
 
 class MinimalTextBoxSchema(Schema):
 
-    start_x: fields.Int()
-    start_y: fields.Int()
-    width: fields.Int()
-    height: fields.Int()
+    start_x = fields.Int(metadata={'description': 'x-axis coordinate of the starting point of the box'})
+    start_y = fields.Int(metadata={'description': 'y-axis coordinate of the starting point of the box'})
+    width = fields.Int(metadata={'description': 'width of the text box'})
+    height = fields.Int(metadata={'description': 'height of the text box'})
+
+
+class ErrorSchema(Schema):
+
+    timestamp = fields.DateTime(metadata={'description': 'Timestamp of when error occurred'})
+    error = fields.Str(metadata={'description': 'Error description'})
