d7dec28a510bcff1e677ddd950815d2f0c2e0922
jmajaca <josip.majaca@gmail.com>
2022-04-17 21:06:47 +0200

    [GOLANG-BACKEND-44] Remove double writing of status code on health endpoint

--- show-off-api/golang/main/main.go
+++ show-off-api/golang/main/main.go
@@ -43,7 +43,6 @@ func health(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	w.WriteHeader(http.StatusOK)
 }
 
 func read(w http.ResponseWriter, r *http.Request) {
