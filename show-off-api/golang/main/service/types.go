0b373b36c79233f3a10b18a089a14bb344ba8a20
jmajaca <josip.majaca@gmail.com>
2022-04-17 19:24:27 +0200

    [GOLANG-BACKEND-44] Fix marshalling of json when sending it to queue

--- show-off-api/golang/main/service/types.go
+++ show-off-api/golang/main/service/types.go
@@ -1,18 +1,18 @@
 package service
 
 type ImageBoxData struct {
-	StartX int    `bson:"start_x"`
-	StartY int    `bson:"start_y"`
-	Width  int    `bson:"width"`
-	Height int    `bson:"height"`
-	Text   string `bson:"text"`
+	StartX int    `json:"start_x"`
+	StartY int    `json:"start_y"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+	Text   string `json:"text"`
 }
 
 type ImageBoxDataList []ImageBoxData
 
 type TextCorrection struct {
-	Id   string `bson:"id"`
-	Text string `bson:"text"`
+	Id   string `json:"id"`
+	Text string `json:"text"`
 }
 
 type ServerConnectionData struct {
